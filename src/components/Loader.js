fa2678661740b3aec6fa832b017df011ca6292b5
adavijit <adavijit@yahoo.com>
2020-02-06 13:53:37 +0530

    5th

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Loader.css';
+export class Loader extends Component {
+    render() {
+        return (
+            <div>
+                <p class="aligncenter">
+    <img id="logo" src="./img/logo-2.png" alt="centered image" height="250px;" width="250px;" alt="im" />
+</p>
+            </div>
+        )
+    }
+}
+
+export default Loader;

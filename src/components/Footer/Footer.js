52a23c433498df1e04b8606b8a554a3d29ded6ca
adavijit <adavijit@yahoo.com>
2020-02-14 23:12:55 +0530

    2nd day

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,18 +6,18 @@ export class Footer extends Component {
            
 <footer className="text-center page-footer">
     <div className="pb-4 pt-4">
-      <a href="https://facebook.com/techtrix.rcciit" >
+      <a href="https://facebook.com/techtrix.rcciit" target="_blank">
         <i className="fab fa-facebook-f mr-3 social-icon"></i>
       </a>
 
-      <a href="https://twitter.com/techtrix_rcciit" >
+      <a href="https://twitter.com/techtrix_rcciit" target="_blank">
         <i className="fab fa-twitter mr-3 social-icon"></i>
       </a>
 
-      <a href="https://instagram.com/techtrix.rcciit" >
+      <a href="https://instagram.com/techtrix.rcciit" target="_blank">
         <i className="fab fa-instagram mr-3 social-icon"></i>
       </a>
-      <a href="https://github.com/swc-rcciit" >
+      <a href="https://github.com/swc-rcciit" target="_blank">
         <i className="fab fa-github mr-3 social-icon"></i>
       </a>
 
